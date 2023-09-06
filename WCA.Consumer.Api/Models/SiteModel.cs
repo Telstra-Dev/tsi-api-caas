86800f7a169ad776265bd3b4a4f2ced7664f87e6
Krishnam <krishnarakesh.kumarverma@team.telstra.com>
2023-09-06 07:18:00 +0000

    Merged PR 10233: Active flag added back to SiteModel
    Active flag added back to SiteModel

--- WCA.Consumer.Api/Models/SiteModel.cs
+++ WCA.Consumer.Api/Models/SiteModel.cs
@@ -6,6 +6,7 @@ namespace WCA.Consumer.Api.Models
     {
         public string SiteId { get; set; }
         public string Name { get; set; }
+        public bool Active { get; set; } = true;
         public SiteMetadata Metadata { get; set; }
         public SiteLocationModel Location { get; set; }
     }
